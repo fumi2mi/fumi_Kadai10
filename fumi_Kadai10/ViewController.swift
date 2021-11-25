5c181070cc3d31bd2226963f294a29add28dce5a
fumi2mi <imafumi.murataka@gmail.com>
2021-11-25 22:17:49 +0900

    Initial Commit

---
+++ fumi_Kadai10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fumi_Kadai10
+//
+//  Created by Fumitaka Imamura on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
