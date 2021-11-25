6f7edacc92ca7516d82d92785c5f42b865e081b7
fumi2mi <imafumi.murataka@gmail.com>
2021-11-25 23:39:11 +0900

    SwiftLint追加修正、private outletwo

--- fumi_Kadai10/TableViewCell.swift
+++ fumi_Kadai10/TableViewCell.swift
@@ -8,18 +8,25 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var commentLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var commentLabel: UILabel!
+
+    func getNameLabel() -> UILabel {
+        return nameLabel
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func getCommentLabel() -> UILabel {
+        return commentLabel
     }
 
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 }
--- fumi_Kadai10/ViewController.swift
+++ fumi_Kadai10/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
+    private let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
                        "山形県", "福島県", "茨城県", "栃木県", "群馬県",
                        "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
                        "富山県", "石川県", "福井県", "山梨県", "長野県",
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
                        "徳島県", "香川県", "愛媛県", "高知県", "福岡県",
                        "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県",
                        "鹿児島県", "沖縄県"]
-    let backGroundColors = [UIColor(red: 255/255, green: 209/255, blue: 208/255, alpha: 1),
+    private let backGroundColors = [UIColor(red: 255/255, green: 209/255, blue: 208/255, alpha: 1),
                             UIColor(red: 222/255, green: 246/255, blue: 154/255, alpha: 1),
                             UIColor(red: 203/255, green: 216/255, blue: 255/255, alpha: 1)]
 }
@@ -31,8 +31,8 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? TableViewCell
         else { return UITableViewCell() }
-        cell.nameLabel.text = prefectures[indexPath.row]
-        cell.commentLabel.text = "\(indexPath.row + 1)番目の都道府県です"
+        cell.getNameLabel().text = prefectures[indexPath.row]
+        cell.getCommentLabel().text = "\(indexPath.row + 1)番目の都道府県です"
         cell.backgroundColor = backGroundColors[indexPath.row % 3]
         return cell
     }
