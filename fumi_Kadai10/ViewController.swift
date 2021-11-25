167a981775c321f5189fce5bad34584c9a5f678b
fumi2mi <imafumi.murataka@gmail.com>
2021-11-25 22:30:24 +0900

    DataSourceをとりあえず記載、TableViewCellのidentifierをCellに設定

--- fumi_Kadai10/ViewController.swift
+++ fumi_Kadai10/ViewController.swift
@@ -22,9 +22,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectures.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
